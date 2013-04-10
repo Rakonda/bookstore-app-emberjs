17097fde5788c267ce9ac7435a6d7af6462cf782
Vytautas Galdikas <galdikas.vytautas@gmail.fake>
2013-04-11 00:46:15 +0100

    RESTFull adapter working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
 App = Ember.Application.create();
+ 
+//Data store
+App.Store = DS.Store.extend({
+        revision:12,
+        adapter:DS.RESTAdapter.create({
+                url:"http://localhost/book-store-restful-api/api"
+        })
+});
+//routers
+App.Router.map(function(){
+    this.resource('books');
+    this.resource('admin');
+});
+ 
+ 
+App.BooksRoute = Ember.Route.extend({
+        model: function(){
+                return App.Book.find();
+        }
+});
+ 
+App.IndexRoute = Ember.Route.extend({
+        redirect: function(){
+                this.transitionTo('books');
+        }
+});
+ 
+App.Book = DS.Model.extend({
+        isbn: DS.attr('string'),
+        title: DS.attr('string'),
+        author: DS.attr('string'),
+        pub: DS.attr('string'),
+        year: DS.attr('string'),
+        price: DS.attr('string')
+});
